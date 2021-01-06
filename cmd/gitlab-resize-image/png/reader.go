c950f31a995de755421b981caa3be9f5184776c9
Matthias Kaeppler <m.kaeppler@gmail.com>
2021-01-06 17:14:04 +0100

    Drop state machine logic and use MultiReaders

--- cmd/gitlab-resize-image/png/reader.go
+++ cmd/gitlab-resize-image/png/reader.go
@@ -1,6 +1,7 @@
 package png
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -9,22 +10,9 @@ import (
 )
 
 const (
-	// We are reading the PNG magic bytes
-	stateReadMagic = iota
-	// We are reading a new PNG chunk
-	stateReadNextChunk
-	// We continue to read an existing chunk
-	stateReadCurrentChunk
-	// We are done skipping potential chunks and let the
-	// underlying reader take over
-	stateDone
-)
-
-const (
-	crcLen         = 4
-	pngMagicLen    = 8
-	chunkHeaderLen = 8
-	pngMagic       = "\x89PNG\r\n\x1a\n"
+	crcLen      = 4
+	pngMagicLen = 8
+	pngMagic    = "\x89PNG\r\n\x1a\n"
 )
 
 // Reader is an io.Reader decorator that skips certain PNG chunks known to cause problems.
@@ -33,39 +21,32 @@ const (
 // See also https://gitlab.com/gitlab-org/gitlab/-/issues/287614
 type Reader struct {
 	underlying     io.Reader
-	state          int
-	buffer         [4096]byte
+	chunkHeader    [8]byte
+	chunkBody      [4096]byte
 	bytesRemaining int
 }
 
-func NewReader(r io.Reader) *Reader {
-	return &Reader{
-		underlying:     r,
-		state:          stateReadMagic,
-		bytesRemaining: 0,
+func NewReader(r io.Reader) io.Reader {
+	magicBytes, err := readMagic(r)
+	if err != nil {
+		panic(err)
 	}
+
+	if string(magicBytes) != pngMagic {
+		debug("Not a PNG - read file unchanged")
+		return io.MultiReader(bytes.NewReader(magicBytes), r)
+	}
+
+	return io.MultiReader(bytes.NewReader(magicBytes), &Reader{underlying: r}, r)
 }
 
 func (r *Reader) Read(p []byte) (n int, err error) {
-	switch r.state {
-	case stateDone:
-		// There is no more work to do, we let the underlying reader take over.
-		debug("Done (forward read)")
-		return r.underlying.Read(p)
-
-	case stateReadMagic:
-		return r.readMagic(p)
-
-	case stateReadNextChunk:
-		return r.readNextChunk(p)
-
-	case stateReadCurrentChunk:
+	if r.bytesRemaining > 0 {
 		// This means in the previous invocation, we weren't able to read
 		// the entire chunk. Keep copying chunk data.
-		return r.copyChunkData(p, r.buffer[:], r.bytesRemaining)
+		return r.copyChunkData(p)
 	}
-
-	return 0, fmt.Errorf("unexpected state: %d", r.state)
+	return r.readNextChunk(p)
 }
 
 func debug(args ...interface{}) {
@@ -75,23 +56,15 @@ func debug(args ...interface{}) {
 }
 
 // Consume PNG magic and proceed to reading the IHDR chunk.
-func (r *Reader) readMagic(dst []byte) (n int, err error) {
+func readMagic(r io.Reader) ([]byte, error) {
 	debug("Read magic")
-	magicBytes := r.buffer[:pngMagicLen]
-	n, err = io.ReadFull(r.underlying, magicBytes)
+	var magicBytes []byte = make([]byte, pngMagicLen)
+	_, err := io.ReadFull(r, magicBytes)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	// Immediately move to done when we're not reading a PNG
-	if string(magicBytes) != pngMagic {
-		debug("Not a PNG - read file unchanged")
-		r.state = stateDone
-	} else {
-		r.state = stateReadNextChunk
-	}
-
-	return copy(dst, magicBytes), nil
+	return magicBytes, nil
 }
 
 // Starts reading a new chunk. We need to look at each chunk between IHDR and PLTE/IDAT
@@ -115,22 +88,23 @@ func (r *Reader) readNextChunk(dst []byte) (int, error) {
 		// This means there was no iCCP chunk and we can just forward all
 		// remaining work to the underlying reader.
 		debug("Encountered", chunkTyp, "(no iCCP chunk found)")
-		n := copy(dst, r.buffer[:chunkHeaderLen])
+		n := copy(dst, r.chunkHeader[:])
 		m, err := r.underlying.Read(dst[n:])
-		r.state = stateDone
-		return n + m, err
+		if err != nil {
+			return n + m, err
+		}
+		return n + m, io.EOF // EOF passes control to the next reader
 
 	default:
 		// iCCP chunk not found yet; we need to remain in this state and read more chunks.
 		debug("read next chunk", chunkTyp)
-		bufferHead := r.buffer[:chunkHeaderLen]
-		bufferTail := r.buffer[chunkHeaderLen:]
 
 		// Copy the chunk header bytes we already read.
-		n := copy(dst, bufferHead)
+		n := copy(dst, r.chunkHeader[:])
 
 		// Copy the remaining bytes.
-		m, err := r.copyChunkData(dst[n:], bufferTail, fullChunkLen)
+		r.bytesRemaining = fullChunkLen
+		m, err := r.copyChunkData(dst[n:])
 		return n + m, err
 	}
 }
@@ -140,42 +114,40 @@ func (r *Reader) readNextChunk(dst []byte) (int, error) {
 func (r *Reader) readChunkLengthAndType() (uint32, string, error) {
 	debug("Read chunk def")
 	// Read chunk length and type.
-	_, err := io.ReadFull(r.underlying, r.buffer[:chunkHeaderLen])
+	_, err := io.ReadFull(r.underlying, r.chunkHeader[:])
 	if err != nil {
 		return 0, "", err
 	}
 
-	chunkLen := binary.BigEndian.Uint32(r.buffer[:4])
-	chunkTyp := string(r.buffer[4:chunkHeaderLen])
+	chunkLen := binary.BigEndian.Uint32(r.chunkHeader[:4])
+	chunkTyp := string(r.chunkHeader[4:])
 
 	debug("LEN:", chunkLen, "TYP:", chunkTyp)
 
 	return chunkLen, chunkTyp, nil
 }
 
-func (r *Reader) copyChunkData(dst []byte, src []byte, remainingBytes int) (int, error) {
+func (r *Reader) copyChunkData(dst []byte) (int, error) {
 	debug("copying chunk data")
 	// Read at most the remaining chunk bytes
 	// OR the number of bytes we can fit into the destination buffer
 	// OR the number of bytes we can fit into the read buffer,
 	// whichever is smallest.
-	lastByte := min(min(remainingBytes, len(src)), len(dst))
-	m, err := io.ReadFull(r.underlying, src[:lastByte])
+	lastByte := min(min(r.bytesRemaining, len(r.chunkBody)), len(dst))
+	m, err := io.ReadFull(r.underlying, r.chunkBody[:lastByte])
 	if err != nil {
 		return m, err
 	}
 
 	// Transfer read buffer contents to destination buffer.
-	m = copy(dst, src[:m])
+	m = copy(dst, r.chunkBody[:m])
 
-	if m < remainingBytes {
+	if m < r.bytesRemaining {
 		// We weren't able to read the full chunk. Keep trying with the next Read.
-		r.bytesRemaining = remainingBytes - m
-		r.state = stateReadCurrentChunk
+		r.bytesRemaining -= m
 	} else {
 		// We read the full chunk so we're ready to read the next.
 		r.bytesRemaining = 0
-		r.state = stateReadNextChunk
 	}
 	debug("bytes remaining:", r.bytesRemaining)
 	return m, nil
